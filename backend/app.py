21fa9452fca6b66e7d45292dd10fad40f0ab7df4
Ayden <ajleu@ucdavis.edu>
2025-05-18 23:43:54 -0700

    cleaning

--- backend/app.py
+++ backend/app.py
@@ -1,34 +1,45 @@
-import os, requests
-from flask import \
-    Flask, jsonify, send_from_directory, request, \
-    redirect, url_for, session
+import os
+import requests
+from flask import Flask, jsonify, send_from_directory, request, redirect, session
 from flask_cors import CORS
 from authlib.integrations.flask_client import OAuth
 from authlib.common.security import generate_token
-
-# load environment variables from .env file
 from dotenv import load_dotenv
+from datetime import datetime
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+
+# load environment variables
 load_dotenv(dotenv_path="./.env")
+FLASK_ENV = os.getenv('FLASK_ENV')
+NYT_API_KEY = os.getenv('NYT_API_KEY')  # should be set now when tester.py tests, hopefully
+MONGO_URI = os.getenv("MONGO_URI")
+STATIC_PATH = os.getenv('STATIC_PATH', 'static')
+TEMPLATE_PATH = os.getenv('TEMPLATE_PATH', 'templates')
+OIDC_CLIENT_ID = os.getenv('OIDC_CLIENT_ID')
+OIDC_CLIENT_SECRET = os.getenv('OIDC_CLIENT_SECRET')
 
-static_path = os.getenv('STATIC_PATH','static')
-template_path = os.getenv('TEMPLATE_PATH','templates')
-
-app = Flask(__name__, static_folder=static_path, template_folder=template_path)
+# flask app setup
+app = Flask(__name__, static_folder=STATIC_PATH, template_folder=TEMPLATE_PATH)
 app.secret_key = os.urandom(24)
+CORS(app)
+
+# authentication with OAuth setup
 oauth = OAuth(app)
 nonce = generate_token()
 
+client_name = os.getenv('OIDC_CLIENT_NAME')
+
 oauth.register(
-    name = os.getenv('OIDC_CLIENT_NAME'),
-    client_id = os.getenv('OIDC_CLIENT_ID'),
-    client_secret = os.getenv('OIDC_CLIENT_SECRET'),
-    #server_metadata_url='http://dex:5556/.well-known/openid-configuration',
-    authorization_endpoint = "http://localhost:5556/auth",
-    token_endpoint = "http://dex:5556/token",
-    jwks_uri = "http://dex:5556/keys",
-    userinfo_endpoint = "http://dex:5556/userinfo",
-    device_authorization_endpoint =" http://dex:5556/device/code",
-    client_kwargs = {'scope': 'openid email profile'}
+    name=client_name,
+    client_id=OIDC_CLIENT_ID,
+    client_secret=OIDC_CLIENT_SECRET,
+    authorization_endpoint="http://localhost:5556/auth",
+    token_endpoint="http://dex:5556/token",
+    jwks_uri="http://dex:5556/keys",
+    userinfo_endpoint="http://dex:5556/userinfo",
+    device_authorization_endpoint="http://dex:5556/device/code",
+    client_kwargs={'scope': 'openid email profile'}
 )
 
 ROLE_MAP = {
@@ -36,235 +47,110 @@ ROLE_MAP = {
     'moderator@hw3.com': 'moderator',
     'user@hw3.com': 'user'
 }
-CORS(app)
 
-# Defined so that we can just call NYT_API_KEY instead of os.getenv('NYT_API_KEY')
-NYT_API_KEY = os.getenv('NYT_API_KEY')
+# mongoDB setup
+mongo = MongoClient(MONGO_URI)
+db = mongo["mydatabase"]
+comments_collection = db["comments"]
+
 
 @app.route('/api/key')
 def get_key():
-    NYT_API_KEY = os.getenv('NYT_API_KEY')  # added because tester.py doesn't call the global version on line 17
     return jsonify({'apiKey': NYT_API_KEY})
 
-# API route that queries the NYT Article Search API for articles related to either Sacramento or Davis, and returns the data.
 @app.route('/api/articles')
 def get_articles():
-    NYT_API_KEY = os.getenv('NYT_API_KEY')  # added because tester.py doesn't call the global version on line 17
+    # page number for infinite scrolling
     page = request.args.get('page', 0, type=int)
-    # Passes the page number to the NYT API (this is for the extra credit requirement of infinite scrolling)
-
-    # Returns an error response if the NYT API key isn't found
-    if NYT_API_KEY == None:
-        return jsonify({'error': 'NYT API key not found'}), 500
     
-    # The endpoint for the NYT Article Search API
+    if not NYT_API_KEY:
+        return jsonify({'error': 'NYT API key not found'}), 500
+
+    # NYT article search API endpoint
     nyt_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
     all_articles = []
 
-    # We tried different combinations of Sacramento and Davis, but the best results were obtained by querying them separately.
-    # We also found that using the default sort order (best) seemed to give us the desired output for both, while newest gave us somewhat inaccurate results.
+    # We found that querying Sacramento and Davis separately gave us the best results.
+    # We also found that using the default sort order gave us more accurate results
+    # compared to sorting by newest.
     try:
-        """
-        By default, the NYT API has a limit of 10 articles per fetch. Since we have 2 fetches (one per city), we can get a maximum of 20 articles.
-        Instead of trying to get all the articles at once, in order to satisfy the extra credit requirement of infinite scrolling, we shall make a new request every time the user scrolls down, which will fetch the next 10 (or technically 20) articles.
-        A timeout of 30 seconds is set in order to avoid being stuck forever.
-        Each request is made, and if successful, gets the json response and stores articles in a list temporarily before adding it to the overall list.
-        .extend() lets us skip having to do a for loop with a .append() for each article.
-        """
-        # First fetch: Sacramento
+        # the NYT API has a limit of 10 articles per fetch.
+        # we have two cities we want articles from, so we can get a maximum of 2*10=20 articles.
+        # instead of calling the API a bunch of times at the start, we call it as the user scrolls down.
+        # a timeout is set so we aren't waiting forever.
+        
+        # sacramento
         sac_articles = fetchArticlesFromLocation(nyt_url, NYT_API_KEY, 'sacramento', 'timesTag.location:"California"')
         all_articles.extend(sac_articles)
-
-        # Second fetch: Davis
+        
+        # davis
         davis_articles = fetchArticlesFromLocation(nyt_url, NYT_API_KEY, 'davis', 'timesTag.organization:"University of California, Davis"')
         all_articles.extend(davis_articles)
-
-        # Since we have all the Davis and Sacramento articles in the same list, we now sort them by the publication date.
+        
+        # sort by date
         combined_articles = sorted(
             all_articles,
             key=lambda x: x.get('pub_date', ''),
-            reverse=True # To ensure that the first articles are the most recent ones
+            reverse=True  # sorts by oldest to newest by default, so we reverse it
         )
-
-        # We don't need all of the information from the articles for our frontend, so we'll prune the data to only include the fields we need.
+        
+        # trim the article data, as we don't need all of it for displaying
         simplified_articles = trimArticleData(combined_articles)
         return jsonify({'articles': simplified_articles})
     
     except requests.RequestException as e:
         return jsonify({'error': str(e)}), 500
 
+# gets user information and converts into json format
 @app.route('/api/user')
-def get_user():
+def get_user():  
     user = session.get('user')
     if user:
         email = user.get('email')
-        role = ROLE_MAP.get(email, 'user')  # Default to 'user' if not found
+        role = ROLE_MAP.get(email, 'user')
         return jsonify({
             'user': {
                 'email': email,
                 'role': role
             }
         })
-    else:
-        return jsonify({'user': None})
+    return jsonify({'user': None})
 
-@app.route('/api/testing/fetchArticles/<path:siteUrl>/<apiKey>/<question>/<fullerQuestion>')
-def fetchArticlesFromLocation(siteUrl:str, apiKey:str, question:str, fullerQuestion:str):
-    response = None
-
-    try:
-        response = requests.get(siteUrl, params={
-            'q': question,
-            'fq': fullerQuestion,
-            'api-key': apiKey
-        }, timeout=30)
-    except requests.RequestException as e:
-        return e
-    
-    if response.status_code == requests.codes.ok:
-        data = response.json()
-        articles = data.get('response', {}).get('docs', [])
-        return articles
-    
-    return []
-
-def trimArticleData(sourceArticles:list):
-    # We don't need all of the information from the articles for our frontend, so we'll prune the data to only include the fields we need.
-    trimmedArticles = []
-
-    for article in sourceArticles:
-        # We need the article headline, of course.
-        headline = article.get('headline', {}).get('main', '')
-        # The abstract is a brief summary of the article. The actual contents we can just link to.
-        abstract = article.get('abstract', '')
-        # We need the URL to link to the article.
-        url = article.get('web_url', '')
-        # We need to show when the article was written.
-        pub_date = article.get('pub_date', '')
-        # Credit to the New York Times. Likely unnecessary since it's implied, but I'll add it for legal reasons.
-        source = article.get('source', '')
-        
-        # We need to display an image for the article. There's a multimedia field containing a list of images, but we only need one for the preview.
-        image_url = None
-        multimedia = article.get('multimedia')
-        
-        # Multimedia field can be a list or a dict, so we'll check both and act accordingly.
-        if isinstance(multimedia, dict):
-            # If it's a dict, we can just check to see if there's a default or thumbnail image defined, since that's what the article would have used in the NYT.
-            if 'default' in multimedia and 'url' in multimedia['default']:
-                image_url = multimedia['default']['url']
-            elif 'thumbnail' in multimedia and 'url' in multimedia['thumbnail']:
-                image_url = multimedia['thumbnail']['url']
-        
-        # If it's a list, we just pick the largest element for ideal visual clarity.
-        # It being a list indicates there's no one image defined as the thumbnail/default image.
-        elif isinstance(multimedia, list) and multimedia:
-            # Pick the largest image from the list
-            valid_images = []
-            for m in multimedia:
-                # If the image has a width and height, it's valid
-                if m.get('width') and m.get('height'):
-                    valid_images.append(m)
-            # If there are any valid images, find and pick the largest one
-            if(valid_images):
-                largest = max(valid_images, key=lambda m: m['width'] * m['height'])
-            # If there's no valid images, we don't have an image to show
-            else:
-                largest = None
-            if largest:
-                # if the image URL already starts with http, that means we can just directly use it
-                if largest['url'].startswith('http'):
-                    image_url = largest['url']
-                # Otherwise, we need to prepend the NYT image URL to the url that we've ascertained
-                else:
-                    image_url = f"https://static01.nyt.com/{largest['url']}"
-        
-        # Append the extracted data to the trimmedArticles list
-        trimmedArticles.append({
-            'headline': headline,
-            'abstract': abstract,
-            'url': url,
-            'pub_date': pub_date,
-            'source': source,
-            'image_url': image_url
-        })
-
-    return trimmedArticles
-
-
-comments_by_url = {}  # Dictionary mapping article URL to list of comments
-
-@app.route('/')
-@app.route('/login')
-def login():
-    session['nonce'] = nonce
-    redirect_uri = 'http://localhost:8000/authorize'
-    return oauth.flask_app.authorize_redirect(redirect_uri, nonce=nonce)
-
-@app.route('/authorize')
-def authorize():
-    token = oauth.flask_app.authorize_access_token()
-    nonce = session.get('nonce')
-
-    user_info = oauth.flask_app.parse_id_token(token, nonce=nonce)  # or use .get('userinfo').json()
-    session['user'] = user_info
-    return redirect('http://localhost:5173')  # dev only
-
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect('/')
-
-@app.route('/<path:path>')
-def serve_frontend(path=''):
-    if path != '' and os.path.exists(os.path.join(static_path,path)):
-        return send_from_directory(static_path, path)
-    return send_from_directory(template_path, 'index.html')
-
-if __name__ == '__main__':
-    debug_mode = os.getenv('FLASK_ENV') != 'production'
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)),debug=debug_mode)
-
-from pymongo import MongoClient
-
-MONGO_URI = os.getenv("MONGO_URI")
-mongo = MongoClient(MONGO_URI)
-db = mongo["mydatabase"]
-comments_collection = db["comments"]
-
+# gets comments for an article from the database and converts into json format
 @app.route('/api/comments', methods=['GET'])
 def get_comments():
     url = request.args.get('url')
     if not url:
         return jsonify({'error': 'Missing article URL'}), 400
 
-    # get comments for this article
     comment_docs = comments_collection.find({'url': url})
     comment_list = []
-    for c in comment_docs:
+    for comment in comment_docs:
         comment_list.append({
-            '_id': str(c.get('_id')),
-            'user': c.get('user'),
-            'text': c.get('text'),
-            'timestamp': c.get('timestamp')  # Optional
+            '_id': str(comment.get('_id')),
+            'user': comment.get('user'),
+            'username': comment.get('username'),
+            'text': comment.get('text'),
+            'timestamp': comment.get('timestamp')
         })
 
     return jsonify({'comments': comment_list})
 
-from datetime import datetime
-
+# receives a comment request and saves it to the database
 @app.route('/api/comments', methods=['POST'])
 def post_comment():
     user = session.get('user')
-    if not user:
+    if not user:  # make sure it's a logged in user
         return jsonify({'error': 'Unauthorized'}), 401
 
     data = request.get_json()
     url = data.get('url')
     text = data.get('text')
 
-    if not url or not text:
+    if not url:
         return jsonify({'error': 'Missing url or text'}), 400
+    if not text:
+        return jsonify({'error': 'Missing text'}), 400
 
     comment = {
         'url': url,
@@ -274,18 +160,9 @@ def post_comment():
     }
 
     result = comments_collection.insert_one(comment)
-    comment['_id'] = str(result.inserted_id)  # Include the inserted ID as a string
+    comment['_id'] = str(result.inserted_id)
 
     return jsonify({'success': True, 'comment': comment})
-
-from bson.objectid import ObjectId
-from flask import abort
-
-def is_moderator():
-    user = session.get('user')
-    return user and user.get('email') in ['moderator@hw3.com', 'admin@hw3.com']
-
-from bson.objectid import ObjectId
 
 @app.route('/api/comments/delete/<comment_id>', methods=['POST'])
 def delete_comment(comment_id):
@@ -297,15 +174,12 @@ def delete_comment(comment_id):
     if not comment:
         return jsonify({'error': 'Comment not found'}), 404
 
-    # Prevent moderators from deleting other moderators' or admin's comments
-    author_email = comment.get('user')
-    if author_email in ['admin@hw3.com', 'moderator@hw3.com'] and user['email'] != author_email:
+    if comment.get('user') in ['admin@hw3.com', 'moderator@hw3.com'] and user['email'] != comment.get('user'):
         return jsonify({'error': 'Cannot delete comments by other moderators/admins'}), 403
 
-    # Perform the soft delete
     result = comments_collection.update_one(
         {'_id': ObjectId(comment_id)},
-        {'$set': {'text': 'COMMENT REMOVED BY MODERATOR'}}
+        {'$set': {'text': 'Comment was removed by a moderator'}}
     )
 
     return jsonify({'success': result.modified_count == 1})
@@ -331,3 +205,82 @@ def redact_comment(comment_id):
         return jsonify({'error': 'Comment not found'}), 404
 
     return jsonify({'success': True})
+
+@app.route('/')
+@app.route('/login')
+def login():
+    session['nonce'] = nonce
+    redirect_uri = 'http://localhost:8000/authorize'
+    return oauth.flask_app.authorize_redirect(redirect_uri, nonce=nonce)
+
+@app.route('/authorize')
+def authorize():
+    token = oauth.flask_app.authorize_access_token()
+    nonce = session.get('nonce')
+    user_info = oauth.flask_app.parse_id_token(token, nonce=nonce)
+    session['user'] = user_info
+    return redirect('http://localhost:5173')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+@app.route('/<path:path>')
+def serve_frontend(path=''):
+    if path != '' and os.path.exists(os.path.join(STATIC_PATH, path)):
+        return send_from_directory(STATIC_PATH, path)
+    return send_from_directory(TEMPLATE_PATH, 'index.html')
+
+@app.route('/api/testing/fetchArticles/<path:siteUrl>/<apiKey>/<question>/<fullerQuestion>')
+def fetchArticlesFromLocation(siteUrl: str, apiKey: str, question: str, fullerQuestion: str):
+    try:
+        response = requests.get(siteUrl, params={
+            'q': question,
+            'fq': fullerQuestion,
+            'api-key': apiKey
+        }, timeout=30)
+        if response.status_code == requests.codes.ok:
+            return response.json().get('response', {}).get('docs', [])
+    except requests.RequestException as e:
+        return e
+    return []
+
+def trimArticleData(sourceArticles: list):
+    trimmedArticles = []
+    for article in sourceArticles:
+        headline = article.get('headline', {}).get('main', '')
+        abstract = article.get('abstract', '')
+        url = article.get('web_url', '')
+        pub_date = article.get('pub_date', '')
+        source = article.get('source', '')
+        image_url = None
+        multimedia = article.get('multimedia')
+
+        if isinstance(multimedia, dict):
+            if 'default' in multimedia and 'url' in multimedia['default']:
+                image_url = multimedia['default']['url']
+            elif 'thumbnail' in multimedia and 'url' in multimedia['thumbnail']:
+                image_url = multimedia['thumbnail']['url']
+        elif isinstance(multimedia, list) and multimedia:
+            valid_images = [m for m in multimedia if m.get('width') and m.get('height')]
+            if valid_images:
+                largest = max(valid_images, key=lambda m: m['width'] * m['height'])
+                if largest['url'].startswith('http'):
+                    image_url = largest['url']
+                else:
+                    image_url = f"https://static01.nyt.com/{largest['url']}"
+
+        trimmedArticles.append({
+            'headline': headline,
+            'abstract': abstract,
+            'url': url,
+            'pub_date': pub_date,
+            'source': source,
+            'image_url': image_url
+        })
+    return trimmedArticles
+
+if __name__ == '__main__':
+    debug_mode = FLASK_ENV != 'production'
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)), debug=debug_mode)
